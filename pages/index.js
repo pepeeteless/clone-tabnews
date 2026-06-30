b0ff9b469b655ea8ddfd4eb994147ef0dcb4f74b
João Pedro Teles <143426491+pepeeteless@users.noreply.github.com>
2026-06-30 16:49:13 -0300

    auto-deploy test

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Maldito o homem que fez eu comprar mais erva de chá por causa de uma piadinha sem graça!</h1>
+    return <h1>Eu te amo!</h1>
 }
 
 export default Home;
