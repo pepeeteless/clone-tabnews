837c133c1dfb3c4430b8d1ad4244adf75738229e
João Pedro Teles <143426491+pepeeteless@users.noreply.github.com>
2026-07-08 17:41:40 -0300

    add `getSSLvalues` function on `database.js`

--- infra/database.js
+++ infra/database.js
@@ -21,7 +21,7 @@ async function getNewClient() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "productions" ? true : false,
+    ssl: getSSLValues(),
   });
 
   await client.connect();
@@ -32,3 +32,12 @@ export default {
   query,
   getNewClient,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "production" ? true : false;
+}
