669c4da34ccb7191ab836f5845ab927a61e6a5a4
pepeeteless <pepeeteless@hotmail.com>
2026-07-29 17:07:08 -0300

    refactor: make controller's pattern

--- infra/database.js
+++ infra/database.js
@@ -1,5 +1,5 @@
 import { Client } from "pg";
-
+import { ServiceError } from "./errors.js";
 async function query(queryObject) {
   let client;
   try {
@@ -7,9 +7,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("\n Erro dentro do catch do database.js");
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      message: "Erro na conexxão ou na Query.",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
